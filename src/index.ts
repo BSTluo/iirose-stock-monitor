23ca2e379db57f42486de26a92b5d378d02c095f
BSTluo <1946831552@qq.com>
2024-01-10 14:52:34 +0800

    modified

--- src/index.ts
+++ src/index.ts
@@ -9,31 +9,44 @@ export interface Config { }
 export const Config: Schema<Config> = Schema.object({});
 
 export function apply(ctx: Context) {
-  let nowData: Stock;
-
-  const status = {
-    down: 0,
-    up: 0,
-    baseMoney: 0,
-    unitPrice: 0,
-    lastBaseMoney: 1,
-    has: 0,
-    new: 0
-  };
+  let tempData: Record<string, {
+    nowData?: Stock;
+    status: { down: number, up: number, baseMoney: number, unitPrice: number, lastBaseMoney: number, has: number, new: number; };
+  }> = {};
 
   // const logger = new Logger('IIROSE-Stock-Monitor');
 
-  ctx.on('iirose/before-getUserList', () => {
-    // logger.warn("接收全局大包...读取股票数据...");
-    ctx.emit('iirose/stockGet', async (data: StockSession) => {
-      if (!nowData)
+  ctx.on('iirose/before-getUserList', (session) => {
+    if (!tempData.hasOwnProperty(session.selfId))
+    {
+      tempData[session.selfId] = {
+        status: {
+          down: 0,
+          up: 0,
+          baseMoney: 0,
+          unitPrice: 0,
+          lastBaseMoney: 1,
+          has: 0,
+          new: 0
+        }
+      };
+    }
+
+    const thisBotObj = tempData[session.selfId]
+    const status = thisBotObj.status;
+
+    // const nowData = thisBotObj.nowData
+
+    session.bot.internal.stockGet(async (data: StockSession) => {
+      if (!thisBotObj.nowData)
       {
-        nowData = data;
+        thisBotObj.nowData = data;
         status.baseMoney = data.personalMoney;
         status.unitPrice = 999999;
+        // console.log(nowData)
       }
 
-      if (nowData == data)
+      if (thisBotObj.nowData == data)
       {
         return;
       }
@@ -48,12 +61,10 @@ export function apply(ctx: Context) {
         // 股票重置
         status.up = 0;
         status.down = 0;
-        nowData = data;
+        thisBotObj.nowData = data;
         // logger.warn(`启动新股${(status.has > 0) ? "已损失" + status.has : ''}`);
         status.has = 0;
         status.new = 1;
-        // console.log(status);
-        nowData = data;
 
         message[2] = `股市崩盘！`;
         message[3] = `股价：${data.unitPrice}`;
@@ -67,20 +78,20 @@ export function apply(ctx: Context) {
         });
       }
 
-      if (data.unitPrice > nowData.unitPrice)
+      if (data.unitPrice > thisBotObj.nowData.unitPrice)
       {
         status.up++;
         status.down = 0;
         message[2] = `已增加${status.up}次`;
-        message[3] = `已增加${(data.unitPrice - nowData.unitPrice).toFixed(2)} 增幅${(((data.unitPrice - nowData.unitPrice) / nowData.unitPrice) * 100).toFixed(2)}%`;
+        message[3] = `已增加${(data.unitPrice - thisBotObj.nowData.unitPrice).toFixed(3)} 增幅${(((data.unitPrice - thisBotObj.nowData.unitPrice) / thisBotObj.nowData.unitPrice) * 100).toFixed(2)}%`;
       }
 
-      if (data.unitPrice < nowData.unitPrice)
+      if (data.unitPrice < thisBotObj.nowData.unitPrice)
       {
         status.up = 0;
         status.down++;
         message[2] = `已降低${status.down}次`;
-        message[3] = `已降低${(nowData.unitPrice - data.unitPrice).toFixed(2)} 降幅${(((nowData.unitPrice - data.unitPrice) / nowData.unitPrice) * 100).toFixed(2)}%`;
+        message[3] = `已降低${(thisBotObj.nowData.unitPrice - data.unitPrice).toFixed(2)} 降幅${(((thisBotObj.nowData.unitPrice - data.unitPrice) / thisBotObj.nowData.unitPrice) * 100).toFixed(2)}%`;
       }
 
       // console.log(status);
@@ -88,7 +99,7 @@ export function apply(ctx: Context) {
       message[5] = `总股：${data.totalStock}`;
       message[6] = `总金：${data.totalMoney}`;
 
-      nowData = data;
+      thisBotObj.nowData = data;
 
       return data.send({
         public: {
@@ -103,5 +114,6 @@ export function apply(ctx: Context) {
       }
       */
     });
+    // logger.warn("接收全局大包...读取股票数据...");
   });
 }
