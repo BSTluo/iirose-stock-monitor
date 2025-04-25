3d42ade5b01713e0f04003afe78d7efa0720c71a
BSTluo <1946831552@qq.com>
2025-04-25 15:02:42 +0800

    更新版本至0.0.8，添加图表显示功能并更新依赖项

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,7 @@
-import { Context, Logger, Schema } from 'koishi';
+import { Context, h, Logger, Schema } from 'koishi';
 import { StockSession } from 'koishi-plugin-adapter-iirose';
 import { Stock } from 'koishi-plugin-adapter-iirose/lib/decoder/Stock';
+import { } from "koishi-plugin-echarts";
 
 export const name = 'iirose-stock-monitor';
 
@@ -8,18 +9,102 @@ export interface Config { }
 
 export const Config: Schema<Config> = Schema.object({});
 
-export function apply(ctx: Context) {
+export const usage = ` # 须知
+v0.0.7版本后，支持图表显示功能，但需要安装echarts插件及其依赖，若不习惯使用，请切换为v0.0.6版本
+`
+
+export const inject = ['echarts'];
+
+export function apply(ctx: Context)
+{
   let tempData: Record<string, {
     nowData?: Stock;
     status: { down: number, up: number, baseMoney: number, unitPrice: number, lastBaseMoney: number, has: number, new: number; };
     isOpen: boolean;
   }> = {};
 
+  let echartsOption = {
+    backgroundColor: 'rgba(254,248,239,1)', // 来自主题
+    color: [
+      "#d87c7c", "#919e8b", "#d7ab82", "#6e7074",
+      "#61a0a8", "#efa18d", "#787464", "#cc7e63",
+      "#724e58", "#4b565b"
+    ],
+    xAxis: {
+      type: 'category',
+      data: [],
+      axisLine: {
+        show: true,
+        lineStyle: {
+          color: '#333333'
+        }
+      },
+      axisTick: {
+        show: true,
+        lineStyle: {
+          color: '#333333'
+        }
+      },
+      axisLabel: {
+        show: true,
+        color: '#333'
+      },
+      splitLine: {
+        show: false
+      }
+    },
+    yAxis: {
+      type: 'value',
+      axisLine: {
+        show: true,
+        lineStyle: {
+          color: '#333'
+        }
+      },
+      axisTick: {
+        show: true,
+        lineStyle: {
+          color: '#333'
+        }
+      },
+      axisLabel: {
+        show: true,
+        color: '#333'
+      },
+      splitLine: {
+        show: true,
+        lineStyle: {
+          color: '#ccc'
+        }
+      }
+    },
+    series: [
+      {
+        data: [],
+        type: 'line',
+        lineStyle: {
+          width: 2
+        },
+        symbol: 'emptyCircle',
+        symbolSize: 8,
+        itemStyle: {
+          borderWidth: 2
+        },
+        smooth: false,
+        label: {
+          show: true,
+          position: 'top'
+        },
+      }
+    ]
+  };
+
   // const logger = new Logger('IIROSE-Stock-Monitor');
 
-  ctx.command('stockMonitor', '花园股票监听器');
+  ctx.command('iirose', '花园工具');
 
-  ctx.command('stockMonitor').subcommand('.on', '开启股票监听功能').action(v => {
+  ctx.command('iirose.stock.on', '开启股票监听功能').action(v =>
+  {
     if (v.session.platform != "iirose") { return; }
     if (!tempData.hasOwnProperty(v.session.selfId))
     {
@@ -43,7 +128,8 @@ export function apply(ctx: Context) {
     v.session.send(' [stockMonitor] 监听已开启');
   });
 
-  ctx.command('stockMonitor').subcommand('.off', '关闭股票监听功能').action(v => {
+  ctx.command('iirose.stock.off', '关闭股票监听功能').action(v =>
+  {
     if (v.session.platform != "iirose") { return; }
     if (!tempData.hasOwnProperty(v.session.selfId))
     {
@@ -67,8 +153,21 @@ export function apply(ctx: Context) {
     v.session.send(' [stockMonitor] 监听已关闭');
   });
 
+  ctx.command('iirose.stock.chart', '查看本轮股票的图表').action(async v =>
+  {
+    if (v.session.platform != "iirose") { return; }
 
-  ctx.on('iirose/before-getUserList', (session) => {
+    const chart = await ctx.echarts.createChart(1000, 700, echartsOption as any);
+
+    const buffer = chart.canvas.toBuffer("image/png");
+    chart.dispose(); // 除非你还想用否则务必销毁实例
+    return [h.text(' [stockMonitor] 本轮股票票价'), h.image(buffer, "image/png")];
+
+  });
+
+
+  ctx.on('iirose/before-getUserList', (session) =>
+  {
     if (!tempData.hasOwnProperty(session.selfId))
     {
       tempData[session.selfId] = {
@@ -93,7 +192,8 @@ export function apply(ctx: Context) {
 
     // const nowData = thisBotObj.nowData
 
-    session.bot.internal.stockGet(async (data: StockSession) => {
+    session.bot.internal.stockGet(async (data: StockSession) =>
+    {
       if (!thisBotObj.nowData)
       {
         thisBotObj.nowData = data;
@@ -127,12 +227,21 @@ export function apply(ctx: Context) {
         message[4] = `总股：${data.totalStock}`;
         message[5] = `总金：${data.totalMoney}`;
 
+        echartsOption.series[0].data =[data.unitPrice];
+        const now = new Date();
+        echartsOption.xAxis.data = [`${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`];
+
         return data.send({
           public: {
             message: message.join('\n')
           }
         });
+
       }
+
+      echartsOption.series[0].data.push(data.unitPrice);
+      const now = new Date();
+      echartsOption.xAxis.data.push(`${now.getHours()}:${now.getMinutes().toString().padStart(2, '0')}`);
 
       if (data.unitPrice > thisBotObj.nowData.unitPrice)
       {
